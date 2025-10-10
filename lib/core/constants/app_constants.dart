183b2c79878cf03ff5ec807e40d7ba8af245cbc5
thang2k6adu <thang2k6adu@gmail.com>
2025-10-10 11:31:02 +0700

    config: create constants & theme

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,10 @@
+class AppConstants {
+  static const String appName = 'My App';
+  static const String apiBaseUrl = 'https://api.example.com';
+  
+  // Secure Storage Keys
+  static const String keyAccessToken = 'access_token';
+  static const String keyRefreshToken = 'refresh_token';
+  static const String keyUserId = 'user_id';
+  static const String keyIsFirstLaunch = 'is_first_launch';
+}
