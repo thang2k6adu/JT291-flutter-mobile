f9a87c77143d498290336c126b097dbd3ab9c1ca
thang2k6adu <thang2k6adu@gmail.com>
2025-10-15 16:20:06 +0700

    refactor: menu_list

--- lib/features/profile/widgets/menu_list.dart
+++ lib/features/profile/widgets/menu_list.dart
@@ -6,45 +6,44 @@ class MenuList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final menuItems = [
+      MenuItemModel(icon: Icons.video_library, title: 'My posts'),
+      MenuItemModel(icon: Icons.backpack_outlined, title: 'My backpack'),
+      MenuItemModel(icon: Icons.groups_outlined, title: 'Task clans'),
+      MenuItemModel(icon: Icons.people_outline, title: 'My referral'),
+      null, // <-- nhóm ngăn cách
+      MenuItemModel(
+        icon: Icons.headset_mic_outlined,
+        title: 'Customer Service',
+      ),
+      MenuItemModel(icon: Icons.help_outline, title: 'Help center'),
+      MenuItemModel(icon: Icons.feedback_outlined, title: 'Feedback'),
+      MenuItemModel(icon: Icons.article_outlined, title: 'Contact Us'),
+    ];
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
-        children: const [
-          CustomMenuItem(
-            icon: Icons.video_library,
-            title: 'My posts',
-          ),
-          CustomMenuItem(
-            icon: Icons.backpack_outlined,
-            title: 'My backpack',
-          ),
-          CustomMenuItem(
-            icon: Icons.groups_outlined,
-            title: 'Task clans',
-          ),
-          CustomMenuItem(
-            icon: Icons.people_outline,
-            title: 'My referral',
-          ),
-          SizedBox(height: 12),
-          CustomMenuItem(
-            icon: Icons.headset_mic_outlined,
-            title: 'Customer Service',
-          ),
-          CustomMenuItem(
-            icon: Icons.help_outline,
-            title: 'Help center',
-          ),
-          CustomMenuItem(
-            icon: Icons.feedback_outlined,
-            title: 'Feedback',
-          ),
-          CustomMenuItem(
-            icon: Icons.article_outlined,
-            title: 'Contract Us',
-          ),
+        children: [
+          for (final item in menuItems)
+            if (item == null)
+              const SizedBox(height: 12)
+            else
+              CustomMenuItem(
+                icon: item.icon,
+                title: item.title,
+                onTap: item.onTap,
+              ),
         ],
       ),
     );
   }
+}
+
+class MenuItemModel {
+  final IconData icon;
+  final String title;
+  final VoidCallback? onTap;
+
+  const MenuItemModel({required this.icon, required this.title, this.onTap});
 }
