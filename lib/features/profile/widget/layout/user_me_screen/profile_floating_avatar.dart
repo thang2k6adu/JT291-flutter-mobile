4ea2c1f3b6ca9feecc94489d718242c7cd9c24cb
thang2k6adu <thang2k6adu@gmail.com>
2025-10-25 16:29:35 +0700

    fix: user avatar was placed in wrong position

--- lib/features/profile/widget/layout/user_me_screen/profile_floating_avatar.dart
+++ lib/features/profile/widget/layout/user_me_screen/profile_floating_avatar.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:jt291_flutter_mobile/features/profile/controllers/profile_controller.dart';
+import 'package:jt291_flutter_mobile/features/profile/controllers/draggable_sheet_controller.dart';
+import 'package:jt291_flutter_mobile/features/profile/constants/profile_constants.dart';
 import 'package:jt291_flutter_mobile/features/profile/widget/layout/user_me_screen/user_header/stats_section/avatar_section.dart';
+import 'package:jt291_flutter_mobile/data/providers/user_general/user_general_provider.dart';
 
 /// Floating avatar component cho Profile Screen
 class ProfileFloatingAvatar extends ConsumerWidget {
@@ -9,19 +11,35 @@ class ProfileFloatingAvatar extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final profileData = ref.watch(profileScreenProvider);
-    final profileController = ref.read(profileControllerProvider.notifier);
+    final draggableState = ref.watch(draggableSheetControllerProvider);
+    final userGeneralAsync = ref.watch(userGeneralProvider);
 
-    final position = profileController.getFloatingAvatarPosition(context);
+    final position = _getFloatingAvatarPosition(context, draggableState.currentExtent);
 
     return Positioned(
       top: position.dy,
       left: position.dx,
-      child: profileData.userGeneralAsync.when(
+      child: userGeneralAsync.when(
         data: (user) => UserAvatarSection(avatarUrl: user?.avatarUrl),
         loading: () => const SizedBox.shrink(),
         error: (error, stack) => const SizedBox.shrink(),
       ),
     );
+  }
+
+  /// Calculate floating avatar position based on current extent
+  Offset _getFloatingAvatarPosition(BuildContext context, double currentExtent) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    
+    final top = screenHeight - 
+        ProfileConstants.avatarAnimationRange * 
+        (currentExtent - ProfileConstants.minChildSize) / 
+        (ProfileConstants.maxChildSize - ProfileConstants.minChildSize) - 
+        ProfileConstants.avatarOffset +10;
+    
+    final left = screenWidth / 2 - ProfileConstants.avatarSize / 2 - 94;
+    
+    return Offset(left, top);
   }
 }
