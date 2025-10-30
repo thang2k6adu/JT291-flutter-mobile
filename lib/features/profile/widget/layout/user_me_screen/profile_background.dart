cbc8b26cfbb4875d80c3218c9e0d177bfeade86f
thang2k6adu <thang2k6adu@gmail.com>
2025-10-30 09:22:15 +0700

    feature: image now can slide & auto slide

--- lib/features/profile/widget/layout/user_me_screen/profile_background.dart
+++ lib/features/profile/widget/layout/user_me_screen/profile_background.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:jt291_flutter_mobile/assets/assets.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 /// Background component cho Profile Screen
 class ProfileBackground extends StatelessWidget {
@@ -7,10 +7,33 @@ class ProfileBackground extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final images = <String>[
+      'lib/assets/images/demo1.jpg',
+      'lib/assets/images/demo2.jpg',
+      'lib/assets/images/demo3.jpg',
+    ];
+
     return Positioned.fill(
-      child: Image.asset(
-        ImagePaths.getPath(ImagePath.profileBackground),
-        fit: BoxFit.cover,
+      child: CarouselSlider.builder(
+        options: CarouselOptions(
+          viewportFraction: 1.0,
+          height: double.infinity,
+          enlargeCenterPage: false,
+          enableInfiniteScroll: true,
+          autoPlay: true,
+          autoPlayInterval: const Duration(seconds: 5),
+          autoPlayAnimationDuration: const Duration(milliseconds: 800),
+          autoPlayCurve: Curves.easeInOut,
+        ),
+        itemCount: images.length,
+        itemBuilder: (context, index, realIndex) {
+          return Image.asset(
+            images[index],
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          );
+        },
       ),
     );
   }
