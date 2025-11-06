c7168e68aae2c4ecc51bba8adc0ead25e35a8b92
thang2k6adu <thang2k6adu@gmail.com>
2025-11-06 17:59:16 +0700

    feature: integrate mock call API

--- lib/features/profile/screens/user_relation_screen.dart
+++ lib/features/profile/screens/user_relation_screen.dart
@@ -1,64 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:jt291_flutter_mobile/components/ui/app_search_field.dart';
 import 'package:jt291_flutter_mobile/features/profile/widget/layout/user_relation_screen/user_relation_appbar.dart';
 import 'package:jt291_flutter_mobile/features/profile/widget/layout/user_relation_screen/user_list_section.dart';
 import 'package:jt291_flutter_mobile/features/profile/widget/layout/user_relation_screen/user_relation_tabbar.dart';
-import 'package:jt291_flutter_mobile/data/mocks/following_mock.dart';
-import 'package:jt291_flutter_mobile/data/models/users/following_model.dart';
-import 'package:jt291_flutter_mobile/features/profile/models/user_relation_model.dart';
-import 'package:jt291_flutter_mobile/data/models/users/follower_model.dart';
-import 'package:jt291_flutter_mobile/data/models/users/friend_model.dart';
-import 'package:jt291_flutter_mobile/data/mocks/follower_mock.dart';
-import 'package:jt291_flutter_mobile/data/mocks/friend_mock.dart';
 import 'package:jt291_flutter_mobile/features/profile/widget/ui/user_relation_screen/user_item_widget.dart';
+import 'package:jt291_flutter_mobile/data/providers/relationship/following_list_notifier.dart';
+import 'package:jt291_flutter_mobile/data/providers/relationship/follower_list_notifier.dart';
+import 'package:jt291_flutter_mobile/data/providers/relationship/friend_list_notifier.dart';
+import 'package:jt291_flutter_mobile/features/profile/models/user_relation_model.dart';
 
-class UserRelationScreen extends StatelessWidget {
-  UserRelationScreen({super.key});
-
-  final List<FollowingModel> followings = followingMock;
-  final List<FollowerModel> followers = followerMock;
-  final List<FriendModel> friends = friendMock;
+class UserRelationScreen extends ConsumerWidget {
+  const UserRelationScreen({super.key});
 
   void _handleUserButtonPressed(UserRelationItem user, UserButtonType type) {
     switch (type) {
       case UserButtonType.friends:
-        print('Open friend profile: ${user.nickname}' + ' ${user.id}');
+        print('Open friend profile: ${user.nickname} ${user.id}');
         break;
       case UserButtonType.following:
-        print('Unfollow user: ${user.nickname}' + ' ${user.id}');
+        print('Unfollow user: ${user.nickname} ${user.id}');
         break;
       case UserButtonType.followBack:
-        print('Follow back user: ${user.nickname}' + ' ${user.id}');
+        print('Follow back user: ${user.nickname} ${user.id}');
         break;
     }
   }
 
   @override
-  Widget build(BuildContext context) {
-    final List<UserRelationItem> followingItems = followings
-        .map((following) => UserRelationItem.fromFollowingModel(following))
-        .toList();
-
-    final List<UserRelationItem> followersItems = followers
-        .map((follower) => UserRelationItem.fromFollowerModel(follower))
-        .toList();
-
-    final List<UserRelationItem> friendsItems = friends
-        .map((friend) => UserRelationItem.fromFriendModel(friend))
-        .toList();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final followingState = ref.watch(followingListProvider);
+    final followerState = ref.watch(followerListProvider);
+    final friendState = ref.watch(friendListProvider);
 
     final TextEditingController searchController = TextEditingController();
 
     return DefaultTabController(
-      length: 3, // 3 tab: Following, Followers, Friends
+      length: 3,
       child: Scaffold(
         backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
         appBar: UserRelationAppBar(
           title: 'Darlene Bears',
           bottom: UserRelationTabBar(
-            followingUsersCount: followings.length,
-            followersUsersCount: followers.length,
-            friendsUsersCount: friends.length,
+            followingUsersCount: followingState.value?.length ?? 0,
+            followersUsersCount: followerState.value?.length ?? 0,
+            friendsUsersCount: friendState.value?.length ?? 0,
           ),
         ),
         body: Column(
@@ -67,25 +53,47 @@ class UserRelationScreen extends StatelessWidget {
               controller: searchController,
               hintText: 'Search users',
             ),
-
-            // y
             Expanded(
               child: TabBarView(
                 children: [
-                  UserListSection(
-                    title: 'Following',
-                    users: followingItems,
-                    onUserButtonPressed: _handleUserButtonPressed,
+                  followingState.when(
+                    data: (list) => UserListSection(
+                      title: 'Following',
+                      users: list
+                          .map((e) => UserRelationItem.fromFollowingModel(e))
+                          .toList(),
+                      onUserButtonPressed: _handleUserButtonPressed,
+                    ),
+                    loading: () =>
+                        const Center(child: CircularProgressIndicator()),
+                    error: (e, st) =>
+                        Center(child: Text('Error loading Following: $e')),
                   ),
-                  UserListSection(
-                    title: 'Followers',
-                    users: followersItems,
-                    onUserButtonPressed: _handleUserButtonPressed,
+                  followerState.when(
+                    data: (list) => UserListSection(
+                      title: 'Followers',
+                      users: list
+                          .map((e) => UserRelationItem.fromFollowerModel(e))
+                          .toList(),
+                      onUserButtonPressed: _handleUserButtonPressed,
+                    ),
+                    loading: () =>
+                        const Center(child: CircularProgressIndicator()),
+                    error: (e, st) =>
+                        Center(child: Text('Error loading Followers: $e')),
                   ),
-                  UserListSection(
-                    title: 'Friends',
-                    users: friendsItems,
-                    onUserButtonPressed: _handleUserButtonPressed,
+                  friendState.when(
+                    data: (list) => UserListSection(
+                      title: 'Friends',
+                      users: list
+                          .map((e) => UserRelationItem.fromFriendModel(e))
+                          .toList(),
+                      onUserButtonPressed: _handleUserButtonPressed,
+                    ),
+                    loading: () =>
+                        const Center(child: CircularProgressIndicator()),
+                    error: (e, st) =>
+                        Center(child: Text('Error loading Friends: $e')),
                   ),
                 ],
               ),
